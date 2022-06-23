d87e4fb0db7dbdda5fa9dc0d0e3edca4e54fb624
HyperCherry <cherryinmc@gmail.com>
2022-06-23 16:15:12 +0800

    Changed main

--- main.py
+++ main.py
@@ -1,8 +1,35 @@
-from hoshino import Service
+from pathlib import Path
+
+from hoshino import Service, R
+from wiki import Wiki, nudge
+
+import wiki
 
 sv = Service('operator_voice')
+
+wiki = Wiki()
+wiki.download_pallas_voices()
+
+
+def get_voice(name: str):
+    oper = '帕拉斯'
+    file = wiki.voice_exists(oper, name)
+    if not file:
+        file = wiki.download_operator_voices(oper, name)
+        if not file:
+            return False
+    return file
 
 
 @sv.on_fullmatch('', only_to_me=True)
 async def demo_fun_1(bot, ev):
     await bot.send(ev, '我在哦博士~')
+    await bot.send(ev, R.rec(Path(get_voice('任命助理'))))
+
+# msg: Message = MessageSegment.record(file=Path(get_voice('任命助理')))
+
+# xxx = R.rec(xxx/xxx.mp3).cqcode
+#
+# @sv.on_fullmatch(["发送语音"])
+# async def xxx(bot, ev):
+#   await bot.send(ev, xxx)
