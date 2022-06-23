c4f026893c6a15dce3ce5b3aaee20c3f52313a37
HyperCherry <cherryinmc@gmail.com>
2022-06-23 19:11:51 +0800

    Added poke voice

--- main.py
+++ main.py
@@ -9,13 +9,23 @@ nudge = ['问候', '闲置', '交谈1', '交谈2', '交谈3',
          '干员报到', '精英化晋升1',
          '编入队伍', '任命队长']
 
+lang = "CHN"
+
 
 @sv.on_fullmatch('', only_to_me=True)
-async def demo_fun_1(bot, ev):
+async def greet(bot, ev):
     await bot.send(ev, '我在哦博士~')
     await bot.send(ev, R.rec(random_voice()).cqcode)
 
 
+def get_voice(target):
+    return lang + "/澄闪_" + target + ".wav"
+
+
 def random_voice():
-    # return "JPN/澄闪_" + choice(nudge) + ".wav"
-    return "CHN/澄闪_" + choice(nudge) + ".wav"
+    return lang + "/澄闪_" + choice(nudge) + ".wav"
+
+
+@sv.on_notice('notify.poke')
+async def poke(bot, ev):
+    await bot.send(ev, R.rec(get_voice("戳一下")).cqcode)
