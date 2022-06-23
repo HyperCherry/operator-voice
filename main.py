866bd9337dc90763c2baf3847f72740ea5a1c940
HyperCherry <cherryinmc@gmail.com>
2022-06-23 15:55:44 +0800

    Changed main

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from hoshino import Service
 sv = Service('operator_voice')
 
 
-@sv.on_fullmatch('你好世界')
-async def hello(bot, ev):
-    await bot.send(ev, '博士我在哦~')
+@sv.on_fullmatch('', only_to_me=True)
+async def demo_fun_1(bot, ev):
+    await bot.send(ev, '我在哦博士~')
